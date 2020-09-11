d810d0c8fd15a97b5ed2b559bc8e575cc8dfe31a
Sreerag n v <31571836+sreeragu555@users.noreply.github.com>
2020-09-11 21:17:47 +0530

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/SignUp',
+      name: 'SignUp',
+      component: SignUp
+    },
+
+  ]
+})
