ba394b65a8db68f2b9430654607e10c1a69a47d1
Sreerag n v <31571836+sreeragu555@users.noreply.github.com>
2020-09-13 21:55:28 +0530

    Add files via upload

---
+++ signup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Signup/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Signup from '@/components/Signup.vue'
+
+export default {
+  name: 'Signup',
+   components:{
+    Signup
+  }
+}
+</script>
